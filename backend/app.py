8362380bd266c06a55ae1807a73f6404ab92d968
Deepanshi <gujjarideepu1@gmail.com>
2026-04-25 22:01:00 +0530

    final project upload

--- backend/app.py
+++ backend/app.py
@@ -5,24 +5,30 @@ from routes.chatbot import chatbot_routes
 from routes.risk import risk_routes
 from routes.report import report_routes
 
+import os
+
 app = Flask(__name__)
 app.secret_key = "secret123"
 
+# Register all routes
 app.register_blueprint(auth_routes)
 app.register_blueprint(attendance_routes)
 app.register_blueprint(chatbot_routes)
 app.register_blueprint(risk_routes)
 app.register_blueprint(report_routes)
 
+# Home
 @app.route('/')
 def home():
     return render_template("login.html")
 
+# Dashboard
 @app.route('/dashboard')
 def dashboard():
     if 'user' in session:
         return render_template("dashboard.html")
     return "Login First ❌"
 
+# Run server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
